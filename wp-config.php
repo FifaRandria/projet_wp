b2fda5e2497e926689ff02ee64e4f0e602d79798
root <root@debian.debian>
2025-12-13 15:24:14 +0300

    improve new page

--- wp-config.php
+++ wp-config.php
@@ -21,11 +21,11 @@ define( 'WP_CACHE', true );
  */
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp4_cash' );
+define( 'DB_NAME', 'wp5_cash' );
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'admin_pma' );
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'MonSuperMotDePasse' );
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
 /** Database charset to use in creating database tables. */
